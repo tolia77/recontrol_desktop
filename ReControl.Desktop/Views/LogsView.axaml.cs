f25e3c73365e1b7fe0ebc6487c5d4b86d9b1d1ad
tolia k <tolia.kohan@gmail.com>
2026-03-27 01:01:49 +0200

    add multi-select and copy support to logs view

--- ReControl.Desktop/Views/LogsView.axaml.cs
+++ ReControl.Desktop/Views/LogsView.axaml.cs
@@ -1,4 +1,7 @@
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using ReControl.Desktop.ViewModels;
 
 namespace ReControl.Desktop.Views;
@@ -18,6 +21,8 @@ public partial class LogsView : UserControl
                 ScrollToBottom();
             }
         };
+
+        KeyDown += OnKeyDown;
     }
 
     private void OnScrollToBottomRequested()
@@ -31,6 +36,33 @@ public partial class LogsView : UserControl
         if (list?.ItemCount > 0)
         {
             list.ScrollIntoView(list.ItemCount - 1);
+        }
+    }
+
+    private void OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.C && e.KeyModifiers.HasFlag(KeyModifiers.Control))
+        {
+            CopySelectedToClipboard();
+            e.Handled = true;
+        }
+    }
+
+    private async void CopySelected_Click(object? sender, RoutedEventArgs e)
+    {
+        CopySelectedToClipboard();
+    }
+
+    private async void CopySelectedToClipboard()
+    {
+        var list = this.FindControl<ListBox>("LogList");
+        if (list?.SelectedItems == null || list.SelectedItems.Count == 0) return;
+
+        var text = string.Join("\n", list.SelectedItems.Cast<string>());
+        var clipboard = TopLevel.GetTopLevel(this)?.Clipboard;
+        if (clipboard != null)
+        {
+            await clipboard.SetTextAsync(text);
         }
     }
 }
